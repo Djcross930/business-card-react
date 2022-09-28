20c51b816fe60294f41b39d1661deb16faba9601
Dan Cross <dancross96@gmail.com>
2022-09-27 23:44:18 -0500

    added image (needs to be changed) and info about myself

---
+++ src/components/Image.js
@@ -0,0 +1,4 @@
+
+export default function MainImage() {
+  return <img src="https://i.pinimg.com/originals/b6/cc/b0/b6ccb09b0cc1de2b2d491798f870ab6d.jpg" className="mainImage" alt="Daniel Cross" />
+}
---
+++ src/components/MyInfo.js
@@ -0,0 +1,3 @@
+export default function MyInfo() {
+  return <info> <h1 className="infoHead--one">Daniel Cross</h1> <p className="infoTwo">Frontend Developer</p> </info>
+}
