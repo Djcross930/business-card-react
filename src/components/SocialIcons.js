dd258bb08aed44829cef767e7e12b6e280fa3310
Dan Cross <dancross96@gmail.com>
2022-09-28 12:41:03 -0500

    business card is completed. Some things I may want to add: add links to each button, remove website, and maybe increase fon in about/interests. and add more substance to the text if I actually want to send it to someone. Also remove temp photo and replace it with a real one.

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -1,7 +1,13 @@
 import Twitter from './TwitterIcon.png'
+import Facebook from './FacebookIcon.png'
+import Instagram from './InstagramIcon.png'
+import Github from './GitHubIcon.png'
 
 export default function MySocials() {
   return <div className="Socials">
-    <img src={Twitter} alt="Twitter" />
+    <img className="SocialOne" src={Twitter} alt="Twitter" />
+    <img className="SocialTwo" src={Facebook} alt="Facebook" />
+    <img className="SocialThree" src={Instagram} alt="Instagram" />
+    <img className="SocialFour" src={Github} alt="Github" />
   </div>
 }
